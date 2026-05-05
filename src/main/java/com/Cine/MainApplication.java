63909c76eb1b51ce1e2a6d5afb420b37e43083f6
Josebanano <josegrandecepeda@gmail.com>
2026-05-04 23:18:02 -0700

    Cambios Julio sobre los modelos

--- src/main/java/com/Cine/MainApplication.java
+++ src/main/java/com/Cine/MainApplication.java
@@ -1,11 +1,6 @@
 package com.Cine;
 
-import com.Cine.models.Direccion;
 import com.Cine.models.Usuario;
-import com.Cine.services.DireccionService;
-import com.Cine.services.UsuarioService;
-import com.Cine.utils.DateUtils;
-import com.Cine.utils.HibernateUtils;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -13,7 +8,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.time.Instant;
-import java.util.List;
 
 public class MainApplication extends Application {
 
@@ -40,10 +34,10 @@ public class MainApplication extends Application {
 //        direccionService.addDireccion(direccion);
 //        direccionService.addDireccion(direccion2);
 //
-    Usuario homero = new Usuario("Homero","J.","Simpson",48);
-        homero.setDireccion(direccion);
-       homero.setFechaUltimoInicio(Instant.now());
-        usuarioService.addUser(homero);
+//    Usuario homero = new Usuario("Homero","J.","Simpson",48);
+//        homero.setDireccion(direccion);
+//       homero.setFechaUltimoInicio(Instant.now());
+//        usuarioService.addUser(homero);
 //
 //        Usuario lisa = new Usuario("Lisa","Simpson","Lopez",10);
 //        lisa.setDireccion(direccion2);
--- src/main/java/com/Cine/controllers/InicioController.java
+++ src/main/java/com/Cine/controllers/InicioController.java
@@ -1,6 +1,14 @@
 package com.Cine.controllers;
+import com.Cine.MainApplication;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class InicioController {
     @FXML
@@ -15,17 +23,26 @@ public class InicioController {
     }
 
     @FXML
-    private void BtnSesionAction() {
-        System.out.println("Click en Iniciar sesión");
+    private void BtnSesionAction(ActionEvent actionEvent) throws IOException {
+        Scene scene = ((Button) actionEvent.getSource()).getScene();
+        Stage stage = (Stage) scene.getWindow();
 
-        // Aquí después puedes abrir login.fxml
-        // cambiar ventana o escena
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("views/sesion_2.fxml"));
+
+        Scene nextScene = new Scene(fxmlLoader.load());
+        stage.setTitle("CineSync - Iniciar Sesión");
+        stage.setScene(nextScene);
     }
 
     @FXML
-    private void BtnRegistroAction() {
-        System.out.println("Click en Registrarse");
+    private void BtnRegistroAction(ActionEvent actionEvent) throws IOException{
+        Scene scene3 = ((Button) actionEvent.getSource()).getScene();
+        Stage stage3 = (Stage) scene3.getWindow();
 
-        // Aquí después puedes abrir registro.fxml
+        FXMLLoader fxmlLoader3 = new FXMLLoader(MainApplication.class.getResource("views/CreaCuenta_3.fxml"));
+
+        Scene nextScene = new Scene(fxmlLoader3.load());
+        stage3.setTitle("CineSync - Crear Cuenta");
+        stage3.setScene(nextScene);
     }
 }
