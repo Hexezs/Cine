2c6323ac19d2feac7dbd4b70558a9b64711bb560
rafael <rafaelmoreno755@gmail.com>
2026-05-10 23:03:26 -0600

    Rafael

--- src/main/java/com/Cine/models/ClasificacionRTC.java
+++ src/main/java/com/Cine/models/ClasificacionRTC.java
@@ -8,22 +8,14 @@ import org.hibernate.annotations.GenericGenerator;
 public class ClasificacionRTC {
 
     private int idClasificacionRTC;
-    private String aa;
-    private String a;
-    private String b;
-    private String b15;
-    private String c;
-    private String d;
+    private String nombre;
+    private String descripcion;
 
     public ClasificacionRTC() {}
 
-    public ClasificacionRTC(String aa, String a, String b, String b15, String c, String d) {
-        this.aa = aa;
-        this.a = a;
-        this.b = b;
-        this.b15 = b15;
-        this.c = c;
-        this.d = d;
+    public ClasificacionRTC(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
     }
 
     @Id
@@ -36,45 +28,20 @@ public class ClasificacionRTC {
     private void setIdClasificacionRTC(int idClasificacionRTC) {
         this.idClasificacionRTC = idClasificacionRTC;
     }
-    public String getAa() {
-        return aa;
+
+    public String getNombre(){
+        return nombre;
     }
 
-    public void setAa(String aa) {
-        this.aa = aa;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
-    public String getA() {
-        return a;
-    }
-    public void setA(String a) {
-        this.a = a;
-    }
-    public String getB() {
-        return b;
-    }
-    public void setB(String b) {
-        this.b = b;
+    public String getDescripcion() {
+        return descripcion;
     }
 
-    public String getB15() {
-        return b15;
-    }
-    public void setB15(String b15) {
-        this.b15 = b15;
-    }
-
-    public String getC() {
-        return c;
-    }
-    public void setC(String c) {
-        this.c = c;
-    }
-    public String getD() {
-        return d;
-    }
-
-    public void setD(String d) {
-        this.d = d;
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
     }
 }
