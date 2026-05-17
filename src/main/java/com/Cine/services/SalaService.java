a7abfd4ae74eba95db61c4d989e37d1b4c0f89dd
rafael <rafaelmoreno755@gmail.com>
2026-05-17 08:35:55 -0600

    Rafael

--- src/main/java/com/Cine/services/SalaService.java
+++ src/main/java/com/Cine/services/SalaService.java
@@ -1,5 +1,21 @@
 package com.Cine.services;
 
-public class SalaService {
+import com.Cine.models.Sala;
+import com.Cine.repository.SalaRepository;
+import java.util.List;
 
+public class SalaService {
+    private final SalaRepository salaRepository = new SalaRepository();
+
+    public void agregarSala(Sala sala){
+        salaRepository.saveSala(sala);
+    }
+
+    public List<Sala> obtenerSalas(){
+        return salaRepository.getAllSalas();
+    }
+
+    public Sala buscarPorId(int idSala){
+        return salaRepository.getSalaByID(idSala);
+    }
 }
