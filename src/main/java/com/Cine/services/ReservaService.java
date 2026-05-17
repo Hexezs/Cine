1a3739e828ba7310ed735a54697a0a6aa5a333c7
rafael <rafaelmoreno755@gmail.com>
2026-05-17 08:49:21 -0600

    Rafael

--- src/main/java/com/Cine/services/ReservaService.java
+++ src/main/java/com/Cine/services/ReservaService.java
@@ -1,5 +1,51 @@
 package com.Cine.services;
 
-public class ReservaService {
+import com.Cine.models.Boleto;
+import com.Cine.models.Cartelera;
+import com.Cine.models.Reserva;
+import com.Cine.models.Usuario;
+import com.Cine.repository.ReservaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
+public class ReservaService {
+    private final ReservaRepository reservaRepository = new ReservaRepository();
+
+    public Reserva procesarCompra(Usuario usuario, Cartelera cartelera, List<Boleto> boletos){
+        if (usuario == null) {
+            throw new RuntimeException("Inicia sesión para reservar");
+        }
+        if (cartelera == null) {
+            throw new RuntimeException("No se seleccionó una función");
+        }
+        if (boletos == null) {
+            throw new RuntimeException("No se seleccionaron asientos");
+        }
+
+        Reserva nuevaReserva = new Reserva();
+        nuevaReserva.setFecha(LocalDate.now());
+        nuevaReserva.setIdusuario(usuario);
+        nuevaReserva.setIdcartelera(cartelera);
+
+        for (Boleto b : boletos) {
+            b.setIdReserva(nuevaReserva);
+        }
+        nuevaReserva.setBoletos(boletos);
+
+        reservaRepository.addReserva(nuevaReserva);
+        return nuevaReserva;
+    }
+
+    public List<Reserva> obtenerReservasPorFuncion(int idCartelera) {
+        return reservaRepository.getReservasByCartelera(idCartelera);
+    }
+
+    public List<Reserva> obtenerTodasLasReservas() {
+        return reservaRepository.getAllReservas();
+    }
+
+    public void cancelarReserva(Reserva reserva) {
+        reservaRepository.removeReserva(reserva);
+    }
 }
