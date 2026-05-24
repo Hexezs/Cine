7a691ddc32113211e8eb287d3217c9def6efb296
rafael <rafaelmoreno755@gmail.com>
2026-05-23 22:54:52 -0600

    Inicio

--- src/main/java/com/Cine/controllers/InicioController.java
+++ src/main/java/com/Cine/controllers/InicioController.java
@@ -1,14 +1,21 @@
 package com.Cine.controllers;
 import com.Cine.MainApplication;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import com.Cine.models.Pelicula;
+import com.Cine.services.PeliculaService;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.List;
 
 public class InicioController {
     @FXML
@@ -18,8 +25,36 @@ public class InicioController {
     private Button BtnRregistro;
 
     @FXML
+    private FlowPane FlowCartelera;
+
+    private final PeliculaService peliculaService = new PeliculaService();
+
+    @FXML
     public void initialize() {
-        System.out.println("Pantalla principal cargada");
+        cargarCartelera();
+    }
+
+    private void cargarCartelera() {
+        List<Pelicula> peliculas = peliculaService.obtenerPeliculasConCartelera();
+
+        for (Pelicula p : peliculas) {
+            VBox card = new VBox();
+            card.setSpacing(5);
+            card.getStyleClass().add("card-pelicula-vista"); // toma el estilo del CSS
+
+            ImageView imgView = new ImageView(
+                    new Image(new ByteArrayInputStream(p.getImagen()))
+            );
+            imgView.setFitWidth(120);
+            imgView.setFitHeight(160);
+            imgView.setPreserveRatio(true);
+
+            Label nombre = new Label(p.getNombre());
+
+            card.getChildren().addAll(imgView, nombre);
+
+            FlowCartelera.getChildren().add(card);
+        }
     }
 
     @FXML
